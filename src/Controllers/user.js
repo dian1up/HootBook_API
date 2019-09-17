7adf31ff6e6e152c35b7e3fdb5fba619eb7f71ea
Fuad Mustamirrul Ishlah 83 <amir24405@gmail.com>
2019-09-17 12:40:01 +0700

    fixing error handling

--- src/Controllers/user.js
+++ src/Controllers/user.js
@@ -3,10 +3,32 @@ const cache = require('../Configs/redis')
 
 const hash = (string) => {
     const crypto = require('crypto-js')
-    return crypto.SHA256(string).toString(crypto.enc.Hex)
-}
-
+    return crypto.SHA256(string)
+      .toString(crypto.enc.Hex)
+  }
+  
 module.exports={
+    registerPartner:(req, res) => {
+        const data = req.body
+        
+        data.password = hash(data.password)
+        userModel.cekPartner(data)
+            .then(result=>{
+                if(result.length === 0) {
+                    return userModel.registerPartner(data)
+                        .then(result=>res.status(200).json({message:'Register successful'}))
+                        .catch(err=>res.status(500).json({message:err}))
+                }else{
+                    return res.status(403).json({
+                        message:'email already taken'
+                    })
+                }
+            })
+            
+            
+            .catch(err=>res.status(500).json({message:err}))
+            
+    },
     getBook: (req, res) => {
         // cache.get("books:0",  (err, obj)=> {}
         cache.hvals("books:0",  (err, obj)=> {
@@ -76,7 +98,7 @@ module.exports={
                             message: 'Login successfull'
                         })
                       })
-                    } else { return responses.dataManipulationResponse(res, 404, 'Email or password is wrong') }
+                    } else { return res.status(404).json({message: 'Email or Password is wrong'}) }
                 }
             )
             .catch(err => {
