99df87aa2f5cef13abd274ae38d09e954b19fd4a
dian candra pribadi <diancandra112@gmail.com>
2019-09-20 08:11:23 +0700

    log

--- src/Middlewares/auth.js
+++ src/Middlewares/auth.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 module.exports = {
   verifyTokenMiddleware: (req, res, next) => {
     const bearerHeader = req.headers['authorization']
-    console.log(bearerHeader)
+    console.log(req.headers, bearerHeader)
     if (bearerHeader !== undefined) {
       const jwt = require('jsonwebtoken')
       const bearer = bearerHeader.split(' ')
